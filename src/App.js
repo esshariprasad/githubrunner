e6a60a274134f01428b2672bf0953950c78e4007
sai shiva hari prasad <esshariprasad@gmail.com>
2024-08-31 10:42:17 -0500

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and trigger
+          Edit <code>src/App.js</code> and main
         </p>
         <a
           className="App-link"
