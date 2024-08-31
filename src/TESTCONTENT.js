34a9cd131b5ce36d10704b297a66518619cf9f6c
sai shiva hari prasad <esshariprasad@gmail.com>
2024-08-31 12:47:17 -0500

    updated runner

--- src/TESTCONTENT.js
+++ src/TESTCONTENT.js
@@ -5,7 +5,7 @@ import React from "react"
     return(
         <>
         <h1>This is test content</h1>
-        <h4>automated deployement</h4>
+        <h4>automated deployement2</h4>
         </>
     )
 
